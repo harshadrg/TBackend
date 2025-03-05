ed1a7abb155cf0b8c02e960e2c7efc4c079d6bbc
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-06 01:33:30 +0530

    feat: add app.js middleware with express, cors, cookiePareser

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:'16kb'}))//limit the import of json data
+app.use(express.urlencoded({extended: true, limit:'16kb'}))//when search url encoder change 'space' into '%20' and more so on
+app.use(express.static())//file folder image store, 'public asset'
+
+app.use(cookieParser())
+
+
+
+export {app}
