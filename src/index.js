bc892e2895fd70fc7246c0385f83a551c7913631
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-06 01:37:55 +0530

    fix: update connectDB integration to handle promise resolution and errors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ import dotenv from "dotenv"
 //and to make import work for dotenv " below step should must be performed"
 // in pak.json in script in dev add {dev:nodemon -r dotenv/config --experimental-json-modules src/script }
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.on("errorr",((error)=>{
+        console.log("ERRORR:", error);
+        throw error
+        
+    }))
+    app.listen(process.env.PORT || 8080, ()=>{
+        console.log(`Server is running on port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed!", err);
+    
+})
