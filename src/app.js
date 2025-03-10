025e917b60aed743a5d5d64918ebfb9900257f3b
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-10 18:44:42 +0530

    fix: update express.static to serve 'public'  folder correctly

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({limit:'16kb'}))//limit the import of json data
 app.use(express.urlencoded({extended: true, limit:'16kb'}))//when search url encoder change 'space' into '%20' and more so on
-app.use(express.static())//file folder image store, 'public asset'
+app.use(express.static("public"))//file folder image store, 'public asset'
 
 app.use(cookieParser())
 
