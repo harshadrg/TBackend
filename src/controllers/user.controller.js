96444bec522bf8ed62a5a10d268ee4b8ef18b321
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-19 21:29:59 +0530

    feat: add user login and logout controllers and generate access, refresh tokens

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,20 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uplodeOnCloudinary } from "../utils/cloudinary.js";
 
+const generateAccessAndRefreshTokens = async(userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave:false})//prevent default validation before saving the tokens 
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"something went wrong while generating Access and Refresh Tokens");        
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     const {userFullName, username, email, phoneNumber, password, address, location}= req.body;
     // const FullName = Object.assign({},userFullName)
@@ -46,7 +60,7 @@ const registerUser = asyncHandler(async (req, res) => {
         username: username.toLowerCase()
     })
     const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
+        "-password -refreshToken -phoneNumber -address -location" 
     )
     if (!createdUser) {
         throw new ApiError(500, "something went wrong when registering user")
@@ -54,5 +68,84 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User Registered Successfully")
     )
+});
+
+const loginUser = asyncHandler(async(req, res)=>{
+
+    const {username, email, phoneNumber, password} = req.body;
+    if (!(username || email || phoneNumber)) {
+        throw new ApiError(400, "username, email or phoneNo is required")
+    }
+    const user = await User.findOne({
+        $or: [{ username }, { email }, { phoneNumber }] 
+    })
+    if (!user) {
+        throw new ApiError(404, "user not found")
+    }
+
+    const isPassowrdValid = await user.isPassowrdCorrect(password)
+    if (!isPassowrdValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const loggedInUser = await User.findById(user._id)
+    .select("-password -refreshToken -phoneNumber -address -location")
+/**
+ * below error is accure due to missing of await (never for for to add awate while database call) 
+ *    TypeError: Converting circular structure to JSON 
+ *    --> starting at object with constructor 'MongoClient'   
+ *    |     property 's' -> object with constructor 'Object'
+ *    |     property 'sessionPool' -> object with constructor 'ServerSessionPool'  
+ *    --- property 'client' closes the circle  
+ */
+    
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
 })
-export {registerUser}
+const logoutUser = asyncHandler(async(req, res)=>{
+    try {
+        await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                $unset: {
+                    refreshToken: ""
+                }
+            },
+            {
+                new:true //
+            }
+        )  
+        const options = {
+            httpOnly:true,
+            secure:true
+        }
+        return res
+        .status(200, {}, "user loggedOut")
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"))
+    } catch (error) {
+        throw new ApiError(401, "fail to logged out")
+    }
+})
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
