441a1e6a554fe5c800e3c5f97e220e68392f1cff
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-19 21:15:59 +0530

    feat: add  user login and logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { VerfyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -13,5 +14,8 @@ router.route("/register").post(
     ]),
     registerUser
 )
+//loggedInUser
+router.route("/login").post(loginUser)
+router.route("/logout").post(VerfyJwt, logoutUser)//add custom middleware to access cookei to allow logout and clear refresh token
 
 export default router
