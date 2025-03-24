23c525f8256068915191b3c60590618d2bbef6cf
harshadrg <harshadgurnule7+github@gmail.com>
2025-03-24 16:19:39 +0530

    fix typo

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 //checking does user have valid token which idicate user is logged in 
-export const VerfyJwt = asyncHandler(async(req, res, next)=>{
+export const VerifyJwt = asyncHandler(async(req, res, next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorizaton")?.replace("Bearer ", "")
         if (!token) {
@@ -11,7 +11,7 @@ export const VerfyJwt = asyncHandler(async(req, res, next)=>{
         }
     
         const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken -phoneNumber -address -location")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
         if (!user) {
             throw new ApiError(401, "Invalid Access Token")
         }
