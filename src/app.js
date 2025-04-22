bbb37ba65e02a5b9ff464eb20566943f77b7c674
harshadrg <harshadgurnule7+github@gmail.com>
2025-04-23 01:38:01 +0530

    Configure product API route

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
+    credentials: true
 }))
 
-app.use(express.json({limit:'16kb'}))//limit the import of json data
-app.use(express.urlencoded({extended: true, limit:'16kb'}))//when search url encoder change 'space' into '%20' and more so on
+app.use(express.json({ limit: '16kb' }))//limit the import of json data
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))//when search url encoder change 'space' into '%20' and more so on
 app.use(express.static("public"))//file folder image store, 'public asset'
 
 app.use(cookieParser())
@@ -19,6 +19,10 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 app.use("/api/v1/users", userRouter)
 
+import productRoute from "./routes/product.routes.js"
+app.use("/api/v1/products", productRoute);
 
 
-export {app}
+
+
+export { app }
