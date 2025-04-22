5b9ee2b90ec949f307bc319624e26cf1b7b5aefa
harshadrg <harshadgurnule7+github@gmail.com>
2025-04-23 01:34:44 +0530

    save product and user images in separate folders on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,18 +1,20 @@
- import { v2 as cloudinary } from "cloudinary";
- import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
- cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
-    api_key: process.env.CLOUDINARY_API_KEY , 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
- });
-
- const uplodeOnCloudinary = async (localFilePath) => {
+});
+//TODO : upload only public_id not fullurl
+const uploadOnCloudinaryU = async (localFilePath) => {
     try {
-        if(!localFilePath) return null;
+        if (!localFilePath) return null;
+
         // upload file on server (i.e cloudinary)
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type:"auto"
+            resource_type: "auto",
+            folder: "Users"
         })
         fs.unlinkSync(localFilePath)
         return response;
@@ -22,6 +24,27 @@
         fs.unlinkSync(localFilePath)//remove the locally save temp file if upload fails
         return null;
     }
- }
+}
+const uploadOnCloudinaryP = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
 
- export {uplodeOnCloudinary}
+        // upload file on server (i.e cloudinary)
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+            folder: "Products"
+        })
+        fs.unlinkSync(localFilePath)
+        return response;
+        //  console.log("image/video is uploaded", response.url);
+        //remove file form local server
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally save temp file if upload fails
+        return null;
+    }
+}
+
+export {
+    uploadOnCloudinaryU,
+    uploadOnCloudinaryP
+}
